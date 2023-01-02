4c7f73530d5b6deae2af1bced9c97510eec99596
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2023-01-02 20:11:38 -0300

    modify: creating first plugin.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,40 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_system(hello_world)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
         .run();
 }
 
 fn hello_world() {
     println!("hello world!");
 }
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello {}!", name.0);
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+            .add_system(hello_world)
+            .add_system(greet_people);
+    }
+}
+
