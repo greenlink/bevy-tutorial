8ccc7b1f291f2fc3df7c6c2e6bfaa7145f4e1213
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2023-01-04 16:27:51 -0300

    modify: intermitent greet people implemented.

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,14 @@ fn main() {
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
-
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
@@ -23,9 +22,11 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello {}!", name.0);
+        }
     }
 }
 
@@ -33,8 +34,8 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-            .add_system(hello_world)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)          
             .add_system(greet_people);
     }
 }
