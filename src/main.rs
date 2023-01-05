c098d8cd4096b189b84fb258921a9f14acab5646
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2023-01-05 11:59:12 -0300

    modify: make the knight run with a timer.

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,46 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_startup_system(setup)
+        .add_system(animate_sprite)
         .run();
 }
 
-#[derive(Component)]
-struct Person;
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
 
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("Hello {}!", name.0);
+fn animate_sprite(time: Res<Time>, 
+    texture_atlases: Res<Assets<TextureAtlas>>, 
+    mut query: Query<(
+    &mut AnimationTimer, 
+    &mut TextureAtlasSprite, 
+    &Handle<TextureAtlas>)>) {
+    for (mut timer, mut sprite, texture_atlas_handle) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
         }
     }
 }
 
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)          
-            .add_system(greet_people);
-    }
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("../assets/chars/knight/Run.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(128.0, 64.0), 2, 4, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..Default::default()
+        },
+        AnimationTimer(Timer::from_seconds(0.075, TimerMode::Repeating))
+    ));
 }
 
